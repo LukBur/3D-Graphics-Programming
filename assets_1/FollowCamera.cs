1bafb23c429f198de3aa39a30239c7d774cde2fb
unknown <2013burl01@gmail.com>
2025-10-10 16:56:54 +0200

    Update assets_1

---
+++ assets_1/FollowCamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+public Transform lookAtTarget;
+
+    void FixedUpdate()
+    {
+        transform.LookAt(lookAtTarget.position);
+    }
+}
